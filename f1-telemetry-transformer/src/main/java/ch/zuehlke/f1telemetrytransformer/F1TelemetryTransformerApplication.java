2e55054c06d9b16186ad6cdde3691f227cce8d9f
schaujer <jerome.schaub@gmx.ch>
2023-11-21 11:08:42 +0100

    Cleanup + initial setup transformer

---
+++ f1-telemetry-transformer/src/main/java/ch/zuehlke/f1telemetrytransformer/F1TelemetryTransformerApplication.java
@@ -0,0 +1,13 @@
+package ch.zuehlke.f1telemetrytransformer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1TelemetryTransformerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(F1TelemetryTransformerApplication.class, args);
+    }
+
+}
---
+++ f1-telemetry-transformer/src/test/java/ch/zuehlke/f1telemetrytransformer/F1TelemetryTransformerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zuehlke.f1telemetrytransformer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1TelemetryTransformerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

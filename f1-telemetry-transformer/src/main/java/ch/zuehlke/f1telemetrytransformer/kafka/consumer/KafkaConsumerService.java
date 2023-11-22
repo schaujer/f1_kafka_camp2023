4ef10779f906fa0c56e6cc2ce6fd3642d67feca8
schaujer <jerome.schaub@gmx.ch>
2023-11-22 15:05:00 +0100

    Handle concurrent sector3 / laptime updates

--- f1-telemetry-transformer/src/main/java/ch/zuehlke/f1telemetrytransformer/kafka/consumer/KafkaConsumerService.java
+++ f1-telemetry-transformer/src/main/java/ch/zuehlke/f1telemetrytransformer/kafka/consumer/KafkaConsumerService.java
@@ -4,6 +4,8 @@ import ch.zuehlke.f1telemetrytransformer.kafka.model.TelemetryUpdateMessage;
 import ch.zuehlke.f1telemetrytransformer.service.WebsocketService;
 import ch.zuehlke.f1telemetrytransformer.service.model.LapTimeUpdate;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
@@ -11,17 +13,18 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class KafkaConsumerService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumerService.class);
     private final WebsocketService websocketService;
 
     @KafkaListener(topics = "${kafka.topics.output.laptimeupdate}", containerFactory = "lapTimeUpdateListenerContainerFactory")
     public void consumeLapTimeMessage(@Payload LapTimeUpdate message) {
-        System.out.println("Consuming LapTimeUpdate Topic:" + message);
+        LOGGER.info("Consuming LapTimeUpdate Topic: " + message);
         websocketService.sendLaptimeUpdate(message);
     }
 
     @KafkaListener(topics = "${kafka.topics.input.telemetryupdate}", containerFactory = "telemetryUpdateListenerContainerFactory")
     public void consumeTelemetryMessage(@Payload TelemetryUpdateMessage message) {
-        System.out.println("Consuming TelemetryUpdate Topic: " + message);
+        LOGGER.info("Consuming TelemetryUpdate Topic: " + message);
         websocketService.sendTelemetryUpdate(message);
     }
 }
